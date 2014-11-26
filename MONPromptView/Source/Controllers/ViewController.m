875cac50a71c463fb474f10d38795cbfa8f1ac97
Mounir Ybanez <rinuom91@gmail.com>
2014-11-26 17:21:10 +0800

    Setup view controller to handle for displaying the prompt view

--- MONPromptView/Source/Controllers/ViewController.m
+++ MONPromptView/Source/Controllers/ViewController.m
@@ -8,14 +8,13 @@
 
 #import "ViewController.h"
 #import "MONPromptView.h"
+#import "UIViewController+LayoutConstraint.h"
 
 @interface ViewController ()
 
 @property (strong, nonatomic) UIButton *promptViewButton;
-@property (strong, nonatomic) MONPromptView *promptView;
 
-- (void)addLayoutConstraintWithVisualFormat:(NSString *)visualFormat views:(NSDictionary *)views;
-- (void)addLayoutConstraintWithVisualformat:(NSString *)visualFormat views:(NSDictionary *)views superview:(UIView *)superview;
+- (void)showPromptView:(UIButton *)button;
 
 @end
 
@@ -26,6 +25,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.promptViewButton];
+    
+    NSDictionary *views = @{ @"promptViewButton": self.promptViewButton };
+    [self addLayoutConstraintWithVisualFormat:@"V:[promptViewButton(70)]" views:views];
+    [self addLayoutConstraintWithVisualFormat:@"H:[promptViewButton(200)]" views:views];
+    [self addLayoutConstraintCenterForView:self.promptViewButton];
+}
+
+#pragma mark -
+#pragma mark - Actions
+
+- (void)showPromptView:(UIButton *)button {
+    MONPromptView *promptView = [[MONPromptView alloc] initWithTitle:@"Error" message:@"Something went wrong." dismissButtonTitle:@"OK"];
+    [promptView showInView:self.view];
 }
 
 #pragma mark -
@@ -35,7 +51,11 @@
     if (!_promptViewButton) {
         _promptViewButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _promptViewButton.translatesAutoresizingMaskIntoConstraints = NO;
+        _promptViewButton.backgroundColor = [UIColor brownColor];
         [_promptViewButton setTitle:@"Show Prompt View" forState:UIControlStateNormal];
+        [_promptViewButton addTarget:self
+                              action:@selector(showPromptView:)
+                    forControlEvents:UIControlEventTouchUpInside];
     }
     return _promptViewButton;
 }
