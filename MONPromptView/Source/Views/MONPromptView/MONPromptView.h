412e0392a194fcc03e6d5197da136dec16d50ea1
Mounir Ybanez <rinuom91@gmail.com>
2014-11-27 14:42:14 +0800

    Added comments

--- MONPromptView/Source/Views/MONPromptView/MONPromptView.h
+++ MONPromptView/Source/Views/MONPromptView/MONPromptView.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
+/// Attribute keys for customizing the dismiss button, title label, and message label
 extern NSString *kMONPromptViewAttribDismissButtonBackgroundColor;
 extern NSString *kMONPromptViewAttribDismissButtonTextColor;
 extern NSString *kMONPromptViewAttribDismissButtonFont;
@@ -20,21 +21,46 @@ extern NSString *kMONPromptViewAttribMessageFont;
 
 @interface MONPromptView : UIView
 
+/// The delegate that implements the methods of the protocl
 @property (strong, nonatomic) id<MONPromptViewDelegate> delegate;
 
+/// The title text of the prompt */
 @property (nonatomic) NSString *title;
+
+/// The message text of the prompt */
 @property (nonatomic) NSString *message;
+
+/// The dismiss button text of the prompt */
 @property (nonatomic) NSString *dismissButtonTitle;
 
+/**
+ * Constructor
+ * @param title Title text of the title of prompt view
+ *        message Message text of the message of prompt view
+ *        dismissButtonTitle Dismiss button text of prompt view
+ * @return Instance of prompt view
+ */
 - (instancetype)initWithTitle:(NSString *)title
                       message:(NSString *)message
            dismissButtonTitle:(NSString *)dismissButtonTitle;
 
+/**
+ * Constructor
+ * @param title Title text of the title of prompt view
+ *        message Message text of the message of prompt view
+ *        dismissButtonTitle Dismiss button text of prompt view
+ *        attributes This holds the info for the custom color and font of prompt view
+ * @return Instance of prompt view
+ */
 - (instancetype)initWithTitle:(NSString *)title
                       message:(NSString *)message
            dismissButtonTitle:(NSString *)dismissButtonTitle
                    attributes:(NSDictionary *)attributes;
 
+/**
+ * Shows the prompt view
+ * @param superview The view where the prompt view is displayed
+ */
 - (void)showInView:(UIView *)superview;
 
 @end
@@ -42,6 +68,11 @@ extern NSString *kMONPromptViewAttribMessageFont;
 @protocol MONPromptViewDelegate <NSObject>
 
 @optional
+
+/**
+ * The method that will be called upon tapping the dismiss button
+ * @param promptView The prompt view that is about to be dismissed
+ */
 - (void)promptViewWillDismiss:(MONPromptView *)promptView;
 
 @end
--- MONPromptView/Source/Views/MONPromptView/MONPromptView.m
+++ MONPromptView/Source/Views/MONPromptView/MONPromptView.m
@@ -26,15 +26,50 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
 
 @interface MONPromptView ()
 
+/// The dismiss button
 @property (strong, nonatomic) UIButton *dismissButton;
+
+/// The title label
 @property (strong, nonatomic) UILabel *titleLabel;
+
+/// The message label
 @property (strong, nonatomic) UILabel *messageLabel;
 
+/// The wrapper view for the prompt view
 @property (strong, nonatomic) UIView *wrapperView;
 
+/**
+ * Gets height of the prompt view depending on the text in title and message labels
+ * @return The height of the prompt view
+ */
 - (CGFloat)getHeight;
+
+/**
+ * Gets the default height of the prompt view
+ * @return The default height of the prompt view
+ */
+- (CGFloat)getDefaultHeight;
+
+/**
+ * The action that will be triggered when the button is tapped
+ * @param button The dismiss button
+ */
 - (void)tapDismiss:(UIButton *)button;
+
+/**
+ * Hides the prompt view
+ */
 - (void)hide;
+
+/**
+ * Sets up the attributes for the custom dismiss button, title label, and message label
+ */
+- (void)setupAttributes:(NSDictionary *)attributes;
+
+/**
+ * Sets up the layout constraints for the prompt view
+ */
+- (void)setupLayoutConstraints;
 
 @end
 
@@ -69,59 +104,14 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
         self.dismissButtonTitle = dismissButtonTitle;
         
         if (attributes) {
-            UIColor *dismissButtonBackgroundColor = [attributes objectForKey:kMONPromptViewAttribDismissButtonBackgroundColor];
-            UIColor *dismissButtonTextColor = [attributes objectForKey:kMONPromptViewAttribDismissButtonTextColor];
-            UIFont *dismissButtonFont = [attributes objectForKey:kMONPromptViewAttribDismissButtonFont];
-            
-            UIColor *titleTextColor = [attributes objectForKey:kMONPromptViewAttribTitleTextColor];
-            UIFont *titleFont = [attributes objectForKey:kMONPromptViewAttribTitleFont];
-            
-            UIColor *messageTextColor = [attributes objectForKey:kMONPromptViewAttribMessageTextColor];
-            UIFont *messageFont = [attributes objectForKey:kMONPromptViewAttribMessageFont];
-            
-            assert_class(dismissButtonBackgroundColor, [UIColor class]);
-            assert_class(dismissButtonTextColor, [UIColor class]);
-            assert_class(dismissButtonFont, [UIFont class]);
-            assert_class(titleTextColor, [UIColor class]);
-            assert_class(titleFont, [UIFont class]);
-            assert_class(messageTextColor, [UIColor class]);
-            assert_class(messageFont, [UIFont class]);
-            
-            if (dismissButtonBackgroundColor) self.dismissButton.backgroundColor = dismissButtonBackgroundColor;
-            if (dismissButtonTextColor) [self.dismissButton setTitleColor:dismissButtonTextColor forState:UIControlStateNormal];
-            if (dismissButtonFont) self.dismissButton.titleLabel.font = dismissButtonFont;
-            if (titleTextColor) self.titleLabel.textColor = titleTextColor;
-            if (titleFont) self.titleLabel.font = titleFont;
-            if (messageTextColor) self.messageLabel.textColor = messageTextColor;
-            if (messageFont) self.messageLabel.font = messageFont;
+            [self setupAttributes:attributes];
         }
         
         [self addSubview:self.titleLabel];
         [self addSubview:self.messageLabel];
         [self addSubview:self.dismissButton];
         
-        NSDictionary *views   = @{ @"dismissButton"         : self.dismissButton,
-                                   @"messageLabel"          : self.messageLabel,
-                                   @"titleLabel"            : self.titleLabel,
-                                   @"promptView"            : self };
-        
-        NSDictionary *metrics = @{ @"dismissButtonHeight"   : @(kPromptViewDismissButtonHeight),
-                                   @"promptViewWidth"       : @(kPromptViewWidth),
-                                   @"promptViewHeight"      : @([self getDefaultHeight]),
-                                   @"messageLabelHeight"    : @(self.messageLabel.font.lineHeight),
-                                   @"titleLabelHeight"      : @(self.titleLabel.font.lineHeight),
-                                   @"topMargin"             : @(kTopMargin),
-                                   @"sideMargin"            : @(kSideMargin) };
-        
-        [self addLayoutConstraintWithVisualFormat:@"H:|[dismissButton]|" views:views];
-        [self addLayoutConstraintWithVisualFormat:@"H:|-(sideMargin)-[titleLabel]-(sideMargin)-|" views:views metrics:metrics];
-        [self addLayoutConstraintWithVisualFormat:@"H:|-(sideMargin)-[messageLabel]-(sideMargin)-|" views:views metrics:metrics];
-        [self addLayoutConstraintWithVisualFormat:@"V:|-(topMargin)-[titleLabel(>=titleLabelHeight)]-(topMargin)-[messageLabel(>=messageLabelHeight)]-(topMargin)-[dismissButton(dismissButtonHeight)]|" views:views metrics:metrics];
-        
-        [self.wrapperView addSubview:self];
-        [self.wrapperView addLayoutConstraintWithVisualFormat:@"H:[promptView(promptViewWidth)]" views:views metrics:metrics];
-        [self.wrapperView addLayoutConstraintWithVisualFormat:@"V:[promptView(>=promptViewHeight)]" views:views metrics:metrics];
-        [self.wrapperView addLayoutConstraintCenterForSubview:self];
+        [self setupLayoutConstraints];
     }
     
     return self;
@@ -130,6 +120,10 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
 #pragma mark -
 #pragma mark - Wrapper View
 
+/**
+ * Creates the wrapper view
+ * @return The wrapper view
+ */
 - (UIView *)wrapperView {
     if (!_wrapperView) {
         _wrapperView = [[UIView alloc] initWithFrame:CGRectZero];
@@ -142,6 +136,10 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
 #pragma mark -
 #pragma mark - Dismiss Button
 
+/**
+ * Creates the dismiss button
+ * @return The dismiss button
+ */
 - (UIButton *)dismissButton {
     if (!_dismissButton) {
         _dismissButton = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -154,10 +152,18 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
     return _dismissButton;
 }
 
+/**
+ * Sets the title text of the dismiss button
+ * @param dismissButtonTitle The title text of the dismiss button
+ */
 - (void)setDismissButtonTitle:(NSString *)dismissButtonTitle {
     [self.dismissButton setTitle:dismissButtonTitle forState:UIControlStateNormal];
 }
 
+/**
+ * Gets the title text of the dismiss button
+ * @return The title text of the dismiss button
+ */
 - (NSString *)dismissButtonTitle {
     return self.dismissButton.titleLabel.text;
 }
@@ -165,6 +171,10 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
 #pragma mark -
 #pragma mark - Title Label
 
+/**
+ * Creates the title label
+ * @return The title label
+ */
 - (UILabel *)titleLabel {
     if (!_titleLabel) {
         _titleLabel = [[UILabel alloc] initWithFrame:CGRectZero];
@@ -178,11 +188,19 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
     return _titleLabel;
 }
 
+/**
+ * Sets the title text of the title label
+ * @param title The title text of the title label
+ */
 - (void)setTitle:(NSString *)title {
     self.titleLabel.text = title;
     [self layoutIfNeeded];
 }
 
+/**
+ * Gets the title text of the title label
+ * @return The title text of the title label
+ */
 - (NSString *)title {
     return self.titleLabel.text;
 }
@@ -190,6 +208,10 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
 #pragma mark -
 #pragma mark - Message Label
 
+/**
+ * Creates the message label
+ * @return The message label
+ */
 - (UILabel *)messageLabel {
     if (!_messageLabel) {
         _messageLabel = [[UILabel alloc] initWithFrame:CGRectZero];
@@ -203,11 +225,19 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
     return _messageLabel;
 }
 
+/**
+ * Sets the message text of the message label
+ * @param message The message text of the message label
+ */
 - (void)setMessage:(NSString *)message {
     self.messageLabel.text = message;
     [self layoutIfNeeded];
 }
 
+/**
+ * Gets the message text of the message label
+ * @return The message text of the message label
+ */
 - (NSString *)message {
     return self.messageLabel.text;
 }
@@ -239,7 +269,6 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
 #pragma mark - Hide Prompt View
 
 - (void)hide {
-
     [UIView animateWithDuration:0.2f animations:^{
         self.wrapperView.alpha = 0.0f;
     } completion:^(BOOL finished) {
@@ -268,6 +297,64 @@ NSString *kMONPromptViewAttribMessageFont = @"kMONPromptViewAttribMessageFont";
         [self.delegate promptViewWillDismiss:self];
     }
     [self hide];
+}
+
+#pragma mark -
+#pragma mark - Set Up Attributes
+
+- (void)setupAttributes:(NSDictionary *)attributes {
+    UIColor *dismissButtonBackgroundColor = [attributes objectForKey:kMONPromptViewAttribDismissButtonBackgroundColor];
+    UIColor *dismissButtonTextColor = [attributes objectForKey:kMONPromptViewAttribDismissButtonTextColor];
+    UIFont *dismissButtonFont = [attributes objectForKey:kMONPromptViewAttribDismissButtonFont];
+    UIColor *titleTextColor = [attributes objectForKey:kMONPromptViewAttribTitleTextColor];
+    UIFont *titleFont = [attributes objectForKey:kMONPromptViewAttribTitleFont];
+    UIColor *messageTextColor = [attributes objectForKey:kMONPromptViewAttribMessageTextColor];
+    UIFont *messageFont = [attributes objectForKey:kMONPromptViewAttribMessageFont];
+    
+    // Assert if the attribute is an instance of class UIColor and UIFont
+    assert_class(dismissButtonBackgroundColor, [UIColor class]);
+    assert_class(dismissButtonTextColor, [UIColor class]);
+    assert_class(dismissButtonFont, [UIFont class]);
+    assert_class(titleTextColor, [UIColor class]);
+    assert_class(titleFont, [UIFont class]);
+    assert_class(messageTextColor, [UIColor class]);
+    assert_class(messageFont, [UIFont class]);
+    
+    if (dismissButtonBackgroundColor) self.dismissButton.backgroundColor = dismissButtonBackgroundColor;
+    if (dismissButtonTextColor) [self.dismissButton setTitleColor:dismissButtonTextColor forState:UIControlStateNormal];
+    if (dismissButtonFont) self.dismissButton.titleLabel.font = dismissButtonFont;
+    if (titleTextColor) self.titleLabel.textColor = titleTextColor;
+    if (titleFont) self.titleLabel.font = titleFont;
+    if (messageTextColor) self.messageLabel.textColor = messageTextColor;
+    if (messageFont) self.messageLabel.font = messageFont;
+}
+
+#pragma mark -
+#pragma mark - Set Up Layout Constraints
+
+- (void)setupLayoutConstraints {
+    NSDictionary *views   = @{ @"dismissButton"         : self.dismissButton,
+                               @"messageLabel"          : self.messageLabel,
+                               @"titleLabel"            : self.titleLabel,
+                               @"promptView"            : self };
+    
+    NSDictionary *metrics = @{ @"dismissButtonHeight"   : @(kPromptViewDismissButtonHeight),
+                               @"promptViewWidth"       : @(kPromptViewWidth),
+                               @"promptViewHeight"      : @([self getDefaultHeight]),
+                               @"messageLabelHeight"    : @(self.messageLabel.font.lineHeight),
+                               @"titleLabelHeight"      : @(self.titleLabel.font.lineHeight),
+                               @"topMargin"             : @(kTopMargin),
+                               @"sideMargin"            : @(kSideMargin) };
+    
+    [self addLayoutConstraintWithVisualFormat:@"H:|[dismissButton]|" views:views];
+    [self addLayoutConstraintWithVisualFormat:@"H:|-(sideMargin)-[titleLabel]-(sideMargin)-|" views:views metrics:metrics];
+    [self addLayoutConstraintWithVisualFormat:@"H:|-(sideMargin)-[messageLabel]-(sideMargin)-|" views:views metrics:metrics];
+    [self addLayoutConstraintWithVisualFormat:@"V:|-(topMargin)-[titleLabel(>=titleLabelHeight)]-(topMargin)-[messageLabel(>=messageLabelHeight)]-(topMargin)-[dismissButton(dismissButtonHeight)]|" views:views metrics:metrics];
+    
+    [self.wrapperView addSubview:self];
+    [self.wrapperView addLayoutConstraintWithVisualFormat:@"H:[promptView(promptViewWidth)]" views:views metrics:metrics];
+    [self.wrapperView addLayoutConstraintWithVisualFormat:@"V:[promptView(>=promptViewHeight)]" views:views metrics:metrics];
+    [self.wrapperView addLayoutConstraintCenterForSubview:self];
 }
 
 @end
