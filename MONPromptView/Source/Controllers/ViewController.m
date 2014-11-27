b1b6ab5dbef3086fca3ddbed935889f8b7058b68
Mounir Ybanez <rinuom91@gmail.com>
2014-11-27 12:14:57 +0800

    Added delegate method on dismiss in prompt view. Created a method to print all available fonts. Added corner radius on prompt view button. Implemented a test for toggling multi-line title and message.

--- MONPromptView/Source/Controllers/ViewController.m
+++ MONPromptView/Source/Controllers/ViewController.m
@@ -10,11 +10,15 @@
 #import "MONPromptView.h"
 #import "UIViewController+LayoutConstraint.h"
 
-@interface ViewController ()
+@interface ViewController () <MONPromptViewDelegate> {
+    int _counter;
+}
 
 @property (strong, nonatomic) UIButton *promptViewButton;
+@property (strong, nonatomic) MONPromptView *promptView;
 
 - (void)showPromptView:(UIButton *)button;
+- (void)printAllFonts;
 
 @end
 
@@ -25,23 +29,52 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self printAllFonts];
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self.view addSubview:self.promptViewButton];
     
     NSDictionary *views = @{ @"promptViewButton": self.promptViewButton };
-    [self addLayoutConstraintWithVisualFormat:@"V:[promptViewButton(70)]" views:views];
+    [self addLayoutConstraintWithVisualFormat:@"V:[promptViewButton(44)]" views:views];
     [self addLayoutConstraintWithVisualFormat:@"H:[promptViewButton(200)]" views:views];
     [self addLayoutConstraintCenterForView:self.promptViewButton];
+}
+
+#pragma mark -
+#pragma mark - Prompt View
+
+- (MONPromptView *)promptView {
+    if (!_promptView) {
+        _promptView =
+        [[MONPromptView alloc] initWithTitle:@"Hello, John Doe!"
+                                     message:@"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                          dismissButtonTitle:@"TRY AGAIN"
+                                  attributes:@{ kMONPromptViewAttribDismissButtonBackgroundColor: [UIColor colorWithRed:40/255.0f green:122/255.0f blue:1.0f alpha:1.0f],
+                                                kMONPromptViewAttribDismissButtonTextColor: [UIColor whiteColor],
+                                                kMONPromptViewAttribDismissButtonFont: [UIFont fontWithName:@"HelveticaNeue-Light" size:14.0f],
+                                                kMONPromptViewAttribMessageFont: [UIFont fontWithName:@"HelveticaNeue-Light" size:16.0f],
+                                                kMONPromptViewAttribTitleFont: [UIFont fontWithName:@"HelveticaNeue-Medium" size:18.0f] }];
+        _promptView.delegate = self;
+    }
+    return _promptView;
 }
 
 #pragma mark -
 #pragma mark - Actions
 
 - (void)showPromptView:(UIButton *)button {
-    MONPromptView *promptView = [[MONPromptView alloc] initWithTitle:@"Error" message:@"Something went wrong." dismissButtonTitle:@"OK"];
-    [promptView showInView:self.view];
+    _counter++;
+    if (_counter % 3 == 0) {
+        self.promptView.title = @"Hello, John Doe!";
+        self.promptView.message = @"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+    } else if (_counter % 3 == 1) {
+        self.promptView.title = @"Hello, John Doe!";
+        self.promptView.message = @"Lorem Ipsum is simply dummy text.";
+    } else if (_counter % 3 == 2) {
+        self.promptView.title = @"Hello, John Doe! Hello, John Doe! Hello, John Doe! Hello, John Doe! Hello, John Doe!";
+        self.promptView.message = @"Lorem Ipsum is simply dummy text.";
+    }
+    [self.promptView showInView:self.view];
 }
 
 #pragma mark -
@@ -52,6 +85,7 @@
         _promptViewButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _promptViewButton.translatesAutoresizingMaskIntoConstraints = NO;
         _promptViewButton.backgroundColor = [UIColor brownColor];
+        _promptViewButton.layer.cornerRadius = 3.0f;
         [_promptViewButton setTitle:@"Show Prompt View" forState:UIControlStateNormal];
         [_promptViewButton addTarget:self
                               action:@selector(showPromptView:)
@@ -61,6 +95,23 @@
 }
 
 #pragma mark -
-#pragma mark - Prompt View
+#pragma mark - Prompt View Delegate
+
+- (void)promptViewWillDismiss:(MONPromptView *)promptView {
+    
+}
+
+#pragma mark -
+#pragma mark - Print All Fonts
+
+- (void)printAllFonts {
+    NSArray *fontFamilies = [UIFont familyNames];
+    
+    for (int i = 0; i < [fontFamilies count]; i++) {
+        NSString *fontFamily = [fontFamilies objectAtIndex:i];
+        NSArray *fontNames = [UIFont fontNamesForFamilyName:[fontFamilies objectAtIndex:i]];
+        NSLog (@"%@: %@", fontFamily, fontNames);
+    }
+}
 
 @end
